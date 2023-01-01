049dd978ca530c265c57504e0f612216cab9a4af
shanedownes <shanedownes01@gmail.com>
2023-01-01 01:29:29 +0000

    Add input state and handleSubmit to Form.tsx

--- frontend/components/Form.tsx
+++ frontend/components/Form.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { FormData } from "../pages/_app";
 
 type FormProps = {
@@ -16,21 +17,29 @@ const Form: React.FC<FormProps> = ({
 	setFormData,
 	formDataKey
 }) => {
+	const [input, setInput] = useState("");
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setFormData({ ...formData, [formDataKey]: input });
+		console.log(input);
+	};
+
 	return (
-		<form className="mt-4 flex gap-5">
+		<form className="mt-4 flex gap-5" onSubmit={(e) => handleSubmit(e)}>
 			<input
 				type="text"
+				value={input}
 				placeholder={placeholder}
-				onChange={(e) =>
-					setFormData({ ...formData, [formDataKey]: e.target.value })
-				}
+				onChange={(e) => setInput(e.target.value)}
 				className="font-regular w-96 rounded-md bg-presto-input-bg pl-6 pr-6 pt-4 pb-4 font-Inter text-lg text-presto-grey"
 			></input>
 			<input
 				type="submit"
 				value={buttonText}
-				className="font-regular rounded-md bg-presto-green pl-8 pr-8 pt-4 pb-4 font-Inter text-lg text-white"
+				className="font-regular cursor-pointer rounded-md bg-presto-green pl-8 pr-8 pt-4 pb-4 font-Inter text-lg text-white hover:opacity-90"
 			/>
+			<p>{input}</p>
 		</form>
 	);
 };

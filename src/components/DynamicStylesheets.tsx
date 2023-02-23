a94a5208ea73e6366fa8963e872301de38c10357
shanedownes <shanedownes01@gmail.com>
2023-02-23 17:51:12 +0000

    refactor: clean up dynamic stylesheets

--- src/components/DynamicStylesheets.tsx
+++ src/components/DynamicStylesheets.tsx
@@ -4,32 +4,20 @@ import Head from "next/head";
 export const DynamicStylesheets = ({
 	starterKits,
 }: DynamicStylesheetsProps) => {
-	return (
-		<Head>
-			<link
-				href={`https://fonts.googleapis.com/css2?family=${starterKits[0].typography.typefaces.display.font}&display=swap`}
-				rel="stylesheet"
-			></link>
-			<link
-				href={`https://fonts.googleapis.com/css2?family=${starterKits[0].typography.typefaces.text.font}&display=swap`}
-				rel="stylesheet"
-			></link>
-			<link
-				href={`https://fonts.googleapis.com/css2?family=${starterKits[1].typography.typefaces.display.font}&display=swap`}
-				rel="stylesheet"
-			></link>
-			<link
-				href={`https://fonts.googleapis.com/css2?family=${starterKits[1].typography.typefaces.text.font}&display=swap`}
-				rel="stylesheet"
-			></link>
-			<link
-				href={`https://fonts.googleapis.com/css2?family=${starterKits[2].typography.typefaces.display.font}&display=swap`}
-				rel="stylesheet"
-			></link>
-			<link
-				href={`https://fonts.googleapis.com/css2?family=${starterKits[2].typography.typefaces.text.font}&display=swap`}
-				rel="stylesheet"
-			></link>
-		</Head>
-	);
+	const links = starterKits
+		.flatMap(({ typography: { typefaces } }) => [
+			{ font: typefaces.display.font, weight: typefaces.display.weight },
+			{ font: typefaces.text.font, weight: typefaces.text.weight },
+		])
+		.map(({ font, weight }) => {
+			return (
+				<link
+					key={`${font}-${weight}`}
+					href={`https://fonts.googleapis.com/css2?family=${font}:wght@${weight}&display=swap`}
+					rel="stylesheet"
+				/>
+			);
+		});
+
+	return <Head>{links}</Head>;
 };

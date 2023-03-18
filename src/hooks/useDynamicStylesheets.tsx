6d5368758b688f1840ea2c85a66f445495e31f0a
shanedownes <shanedownes01@gmail.com>
2023-03-18 02:19:17 +0000

    fix: add dynamic font stylesheets to dom without using next/head
    https://nextjs.org/docs/messages/no-stylesheets-in-head-component

---
+++ src/hooks/useDynamicStylesheets.tsx
@@ -0,0 +1,23 @@
+import { StarterKits } from "../types/StarterKits";
+import { KITS_COUNT } from "../constants/global";
+import { useEffect } from "react";
+
+const addFontLink = (font: string, weight: string) => {
+	const link = document.createElement("link");
+	link.href = `https://fonts.googleapis.com/css2?family=${font}${
+		weight ? `:wght@${weight}` : ""
+	}&display=swap`;
+	link.rel = "stylesheet";
+	document.head.appendChild(link);
+};
+
+export const useDynamicStylesheets = (starterKits: StarterKits) => {
+	useEffect(() => {
+		if (starterKits.length === KITS_COUNT) {
+			starterKits.forEach(({ typography: { typefaces } }) => {
+				addFontLink(typefaces.display.font, typefaces.display.weight || "");
+				addFontLink(typefaces.text.font, typefaces.text.weight || "");
+			});
+		}
+	}, [starterKits]);
+};

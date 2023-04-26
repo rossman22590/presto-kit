50270d6414d8a504c7d59a2f522115fb61b51867
shanedownes <shanedownes01@gmail.com>
2023-04-26 23:32:57 +0100

    feat: Add get kits colors and fonts query utils

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -1,7 +1,82 @@
-import type { Kits, UploadColors, UploadTypefaces } from "../types/Data";
-import type { Kit } from "../types/Kits";
-import { SupabaseClient } from "@supabase/auth-helpers-react";
+import { SupabaseClient, User } from "@supabase/auth-helpers-react";
 import { getColorCategory } from "./helpers";
+import type { Kit } from "../types/Kits";
+import type {
+	ColorsInsert,
+	ColorsResponse,
+	Kits,
+	KitsResponse,
+	TypefacesInsert,
+	TypographyResponse,
+} from "../types/Data";
+
+export const getKitsByCategory = async (
+	category: Kits["category"],
+	user: User | null,
+	supabase: SupabaseClient
+): Promise<KitsResponse[] | null> => {
+	try {
+		if (!user) throw new Error("No user at get kit");
+
+		let { data, error } = await supabase
+			.from("kits")
+			.select("id, project_id, title")
+			.eq("user_id", user.id)
+			.eq("category", category);
+
+		if (error) throw error;
+
+		return data;
+	} catch (error) {
+		alert("Error loading user data!");
+		console.log(error);
+	}
+	return null;
+};
+
+export const getColorsByKitId = async (
+	kitId: number | null,
+	supabase: SupabaseClient
+): Promise<ColorsResponse[] | null> => {
+	try {
+		if (!kitId) throw new Error("No kit id at get colors");
+
+		let { data, error } = await supabase
+			.from("colors")
+			.select("category, name, hex")
+			.eq("kit_id", kitId);
+
+		if (error) throw error;
+
+		return data;
+	} catch (error) {
+		alert("Error loading color data!");
+		console.log(error);
+	}
+	return null;
+};
+
+export const getTypographyByKitId = async (
+	kitId: number | null,
+	supabase: SupabaseClient
+): Promise<TypographyResponse[] | null> => {
+	try {
+		if (!kitId) throw new Error("No kit id at get typography");
+
+		let { data, error } = await supabase
+			.from("typefaces")
+			.select("category, font, weight")
+			.eq("kit_id", kitId);
+
+		if (error) throw error;
+
+		return data;
+	} catch (error) {
+		alert("Error loading typography data!");
+		console.log(error);
+	}
+	return null;
+};
 
 export const uploadTypography = async (
 	kitId: Kits["id"],
@@ -10,7 +85,7 @@ export const uploadTypography = async (
 	supabase: SupabaseClient
 ) => {
 	try {
-		const typefaceData: UploadTypefaces[] = [
+		const typefaceData: TypefacesInsert[] = [
 			{
 				category: "DISPLAY",
 				font: display.font,
@@ -40,7 +115,7 @@ export const uploadColors = async (
 	supabase: SupabaseClient
 ) => {
 	try {
-		const colorData: UploadColors[] = colors.details.map((color, i) => ({
+		const colorData: ColorsInsert[] = colors.details.map((color, i) => ({
 			category: getColorCategory(i),
 			name: color.name,
 			hex: color.hex,
